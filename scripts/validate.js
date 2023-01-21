180ecee9178b6ed528684059a6290cc3207e35d6
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-01-21 11:04:38 +0300

    feat: make showInputError()

--- scripts/validate.js
+++ scripts/validate.js
@@ -6,14 +6,15 @@ function getErrorClassByInputId(inputElement) {
   return '.' + inputElement.id + '-error';
 }
 
-function showInputError(formElement, inputElement, errorMessage) {
+function showInputError(formElement, inputElement, errorMessage, inputErrorClass, errorClass) {
   const errorElement = formElement.querySelector(getErrorClassByInputId(inputElement));
-  inputElement.classList.add('popup__input_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__error_visible');
+  inputElement.classList.add(inputErrorClass);
+  // ! remove ''
+  errorElement.textContent = 'errorMessage';
+  errorElement.classList.add(errorClass);
 }
 
-showInputError(editProfileForm, profileNameInput, profileNameInput.validationMessage);
+showInputError(editProfileForm, profileNameInput, profileNameInput.validationMessage, 'popup__input_type_error', 'popup__error_visible');
 
 // enableValidation({
 //   formSelector: '.popup__form',
