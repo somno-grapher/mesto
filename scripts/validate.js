ba63dc6ed4ddbfe7532589a1cacf1092b7d1268b
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-01-21 10:54:27 +0300

    feat: make getErrorClassByInputId()

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,10 +2,14 @@
 //   showInputError(editProfileForm, profileAboutInput, profileAboutInput.validationMessage);
 // }
 
+function getErrorClassByInputId(inputElement) {
+  return '.' + inputElement.id + '-error';
+}
+
 function showInputError(formElement, inputElement, errorMessage) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(getErrorClassByInputId(inputElement));
   inputElement.classList.add('popup__input_type_error');
-  errorElement.textContent = 'Вы пропустили это поле errorMessage ffffffffffffffffffffffffffffffffffffffffffff';
+  errorElement.textContent = errorMessage;
   errorElement.classList.add('popup__error_visible');
 }
 
@@ -14,8 +18,8 @@ showInputError(editProfileForm, profileNameInput, profileNameInput.validationMes
 // enableValidation({
 //   formSelector: '.popup__form',
 //   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__button',
-//   inactiveButtonClass: 'popup__button_disabled',
+//   submitButtonSelector: '.popup__save-button',
+//   inactiveButtonClass: 'popup__save-button_disabled',
 //   inputErrorClass: 'popup__input_type_error',
 //   errorClass: 'popup__error_visible'
 // });
