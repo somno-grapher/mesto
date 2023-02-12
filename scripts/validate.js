6299d51373224d0967557f61c129577aa09149cb
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-02-12 15:56:26 +0300

    refactor: implement FormValidator class (no validation on opening)

--- scripts/validate.js
+++ scripts/validate.js
@@ -97,4 +97,92 @@ editProfileButton.addEventListener('click', () => {
 
 // * main code
 
-enableValidation(validationSettings);
+// enableValidation(validationSettings);
+
+// ! class FormValidator
+class FormValidator {
+  constructor(settings, formElement) {
+    this._settings = settings;
+    this._formElement = formElement;
+    this._buttonElement = this._formElement.querySelector(this._settings.saveButtonSelector);
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._settings.inputSelector));
+
+  }
+
+  _addInputListeners() {
+    // toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._isInputValid(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  _getErrorClassByInputId(inputElement) {
+    return '.' + inputElement.id + '-error';
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    })
+  };
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(this._getErrorClassByInputId(inputElement));
+    inputElement.classList.remove(this._settings.inputErrorClass);
+    errorElement.textContent = '';
+    errorElement.classList.remove(this._settings.errorClass);
+  }
+
+  _isInputValid(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _showInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(this._getErrorClassByInputId(inputElement));
+    const errorMessage = inputElement.validationMessage;
+    inputElement.classList.add(this._settings.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._settings.errorClass);
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._settings.inactiveButtonClass);
+      this._buttonElement.type = 'button';
+    } else {
+      this._buttonElement.classList.remove(this._settings.inactiveButtonClass);
+      this._buttonElement.type = 'submit';
+    }
+  };
+
+  // function validateFormOnOpening(formElement, inputErrorClass, errorClass, inputSelector, saveButtonSelector, inactiveButtonClass) {
+  //   const buttonElement = formElement.querySelector(saveButtonSelector);
+  //   const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  //   inputList.forEach((inputElement) => {
+  //     hideInputError(formElement, inputElement, inputErrorClass, errorClass);
+  //   });
+  //   toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+  // };
+
+  enableValidation() {
+    this._addInputListeners();
+  }
+}
+
+function enableValidation2(settings) {
+  const formList = Array.from(document.querySelectorAll(settings.formSelector));
+  formList.forEach((formElement) => {
+    const formValidator = new FormValidator(settings, formElement);
+    formValidator.enableValidation();
+  });
+};
+
+enableValidation2(validationSettings);
+
