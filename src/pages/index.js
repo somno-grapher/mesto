20b4d02d3731eb21a02a711960150c663f121233
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-02-26 11:24:08 +0300

    refactor: reorganize file structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-import Section from './Section.js';
-import PopupWithForm from './PopupWithForm.js';
-import PopupWithImage from './PopupWithImage.js';
-import UserInfo from './UserInfo.js';
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import UserInfo from '../components/UserInfo.js';
 
 // * vars: pseudo-ascending order
 
