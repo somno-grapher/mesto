1a101bcbd847a85b8b639826962c406ec6c74066
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-02-26 14:02:14 +0300

    refactor: handle images

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,11 @@ module.exports = {
       {
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: /node_modules/
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
       }
     ]
   },
