d6545b41cfb06d07bd862f9d0ca01a1a73949744
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-02-28 13:42:14 +0300

    refactor: refactor PopupWithImage constructor

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,9 +2,13 @@ import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
 
-  open(item) {
+  constructor(selector) {
+    super(selector);
     this._photo = this._popupElement.querySelector('.full-photo');
     this._title = this._popupElement.querySelector('.popup__title');
+  }
+
+  open(item) {
     this._photo.src = item.link;
     this._photo.alt = item.name;
     this._title.textContent = item.name;
