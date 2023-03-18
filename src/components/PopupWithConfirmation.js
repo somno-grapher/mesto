ea4b229845ba287702c05cdd2234d9f16de83d52
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-03-18 18:44:32 +0300

    style: remove comments in PopupWithConfirmation

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -4,15 +4,8 @@ export default class PopupWithConfirmation extends Popup {
   constructor(selector) {
     super(selector);
     this._form = this._popupElement.querySelector('form');
-    // this._buttonConfirm = this._form.querySelector('.save-button');
-    // this._buttonConfirmNormalTextContent = this._buttonConfirm.textContent;
     this._submitFormHandler = null;
   }
-
-  // close() {
-  //   super.close();
-    // this._releaseBusyState();
-  // }
 
   open(submitFormHandler) {
     super.open();
@@ -24,17 +17,8 @@ export default class PopupWithConfirmation extends Popup {
     this._form.addEventListener('submit', (event) => this._submitForm(event));
   }
 
-  // _setBusyState() {
-  //   this._buttonConfirm.textContent = "Сохранение...";
-  // }
-
-  // _releaseBusyState() {
-  //   this._buttonConfirm.textContent = this._buttonConfirmNormalTextContent;
-  // }
-
   _submitForm(event) {
     event.preventDefault();
-    // this._setBusyState();
     this._submitFormHandler();
     this.close();
   }
